f04fa49409ea6199f504a58d3104583b2492b41f
nadayassin19 <yassinada19@gmail.com>
2023-12-10 13:45:16 +0200

    test_amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,3 +1,34 @@
 #!/usr/bin/python3
 """A module of unitest testing Amenity class
 """
+import unittest
+from models.amenity import Amenity
+import datetime
+
+
+class TestAmenity(unittest.TestCase):
+    """Unitest testing of class Amenity
+    """
+    my_amenity = Amenity()
+
+    def test_class_exists(self):
+        res = "<class 'models.amenity.Amenity'>"
+        self.assertEqual(str(type(self.my_amenity)), res)
+
+    def test_user_inheritance(self):
+        self.assertIsInstance(self.my_amenity, Amenity)
+
+    def testHasAttributes(self):
+        self.assertTrue(hasattr(self.my_amenity, 'name'))
+        self.assertTrue(hasattr(self.my_amenity, 'id'))
+        self.assertTrue(hasattr(self.my_amenity, 'created_at'))
+        self.assertTrue(hasattr(self.my_amenity, 'updated_at'))
+
+    def test_types(self):
+        self.assertIsInstance(self.my_amenity.name, str)
+        self.assertIsInstance(self.my_amenity.id, str)
+        self.assertIsInstance(self.my_amenity.created_at, datetime.datetime)
+        self.assertIsInstance(self.my_amenity.updated_at, datetime.datetime)
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """A module of unitest testing User class
 """
-import models
-import os
 import unittest
+import models
 from models.user import User
+import os
 from datetime import datetime
 from time import sleep
 
